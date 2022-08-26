86609886e70bfb9ee3e9b8a5b9d8fbc21c0f3803
Vijayakumar K <11499@ramco.com>
2022-08-26 13:47:57 +0530

    NJsonSchema sample initial commit

---
+++ NJsonSchema/Products.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NJsonSchema
+{
+    public class Products
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int ProductCategoryId { get; set; }
+        public int CategoryId { get; set; }
+        [Required]
+        public double MaxRetailPrice { get;set; }
+        [Required]
+        public double UnitPrice { get; set; }
+    }
+}
---
+++ NJsonSchema/ProductsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace NJsonSchema
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase 
+    {
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetProducts")]
+        public IEnumerable<Products> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Products
+            {
+                
+            })
+            .ToArray();
+        }
+
+        [HttpPost(Name = "Products")]
+        public async Task<IActionResult> CreateProduct([FromBody] JObject jObect)
+        {
+            var schema = JsonSchema.FromType<Products>();
+            var schemaData = schema.ToJson();
+            var errors = schema.Validate(jObect.ToString());
+
+            foreach (var error in errors)
+                Console.WriteLine(error.Path + ": " + error.Kind);
+            if (errors.Count > 0)
+            {
+                return BadRequest(errors);
+            }
+            schema = await JsonSchema.FromJsonAsync(schemaData);
+            return Ok(schema);
+        }
+    }
+}
